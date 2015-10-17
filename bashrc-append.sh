17f8435cd256c9140036d5e0bda13514ca20f270
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-10-17 10:21:58 +0200

    Initial commit

---
+++ bashrc-append.sh
@@ -0,0 +1,151 @@
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+	alias dir='dir --color=auto'
+	alias vdir='vdir --color=auto'
+
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
+fi
+
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+
+
+# Marks/jumping
+
+export MARKPATH=$HOME/.marks
+
+function jump {
+    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+}
+
+function mark {
+    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+}
+
+function unmark {
+    rm -i "$MARKPATH/$1"
+}
+
+function marks {
+    ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+}
+
+_completemarks() {
+	local curw=${COMP_WORDS[COMP_CWORD]}
+	local wordlist=$(find $MARKPATH -type l -printf "%f\n")
+	COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+	return 0
+}
+
+complete -F _completemarks jump unmark
+alias jmp=jump
+
+
+## JDK setup ##
+
+setup_jdk() {
+	# Remove the current JDK from PATH
+	if [ -n "$JAVA_HOME" ] ; then
+		PATH=${PATH/$JAVA_HOME\/bin:/}
+	fi
+	export JAVA_HOME=$1
+	export PATH=$JAVA_HOME/bin:$PATH
+}
+
+use_java6() {
+	setup_jdk /usr/lib/jvm/jdk1.6.0_45
+}
+
+use_java7() {
+	setup_jdk /usr/lib/jvm/java-7-openjdk-amd64
+}
+
+
+# git aliases
+
+current-branch() {
+	current_branch=$(git symbolic-ref -q HEAD)
+	current_branch=${current_branch##refs/heads/}
+	current_branch=${current_branch:-HEAD}
+	printf $current_branch
+}
+
+alias gaa='git add -A .'
+
+gcm() {
+	if git status | grep Untracked > /dev/null; then
+		git status
+	else
+		git commit -am "$*"
+	fi
+}
+
+ginit() {
+	git init
+	git add .
+	git commit -m 'Initial commit'
+}
+
+alias gs='git status'
+
+onto() {
+	base_commit=$1
+	target_branch=lmp-dev$2
+	git rebase --onto $target_branch $base_commit $(current-branch)
+}
+
+
+# Misc aliases
+
+function .. {
+	if [ -z $1 ]; then
+		cd ..;
+	else
+		for i in `seq 1 $1`; do
+			cd ..;
+		done
+	fi
+}
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+copy() {
+	buf=/tmp/__buffer
+	rm -rf $buf
+	mkdir $buf
+	cp -r $1 $buf
+}
+
+alias cp='cp -i'
+
+alias gri='grep -Ri'
+
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+
+alias mntdb='sudo mount -t vmhgfs .host:/Dropbox ~/Dropbox'
+
+alias mv='mv -i'
+
+paste() {
+	cp -ir /tmp/__buffer/* .
+}
+
+alias rm='rm -i'
+
+alias sagi='sudo apt-get install'
+
+alias vimba='vim ~/.bashrc; . ~/.bashrc'
+
---
+++ vimrc
@@ -0,0 +1,143 @@
+"""
+"   GENERAL
+"""
+set nocompatible
+filetype plugin on
+
+set nobackup
+set tabstop=4
+set shiftwidth=4
+set noexpandtab
+set autoindent
+
+set showcmd
+set autowrite
+set number
+set nobackup
+set scrolloff=10
+
+" always show status bar
+set laststatus=2
+
+" folding
+"set foldmethod=syntax
+set foldmethod=manual
+set foldnestmax=10
+set nofoldenable
+set foldlevelstart=1
+
+let sh_fold_enabled=1
+let vimsyn_folding='af'
+let xml_syntax_folding=1
+
+" wildcard tab-completion
+set wildmode=longest,list,full
+set wildmenu
+
+" file encoding
+set fenc=utf8
+set enc=utf8
+
+" syntax coloring
+syn on
+
+set completeopt=longest,menuone
+
+" recursive cwd search
+set path+=**
+
+" persistent undo
+"if has('undofile')
+    set undofile
+    set undodir=$HOME/.vim/undo
+    set undolevels=1000
+    set undoreload=10000
+"endif
+
+" automatic cwd change for each file
+"autocmd BufEnter * silent! lcd %:p:h
+
+" line at 80 chars + highlight lines longer
+"if exists('+colorcolumn')
+"    set colorcolumn=80
+"else
+"    au BufWinEnter * let w:m2=matchadd('ErrorMsg', '\%80v.\+', -1)
+"endif
+"
+"highlight OverLength ctermbg=red ctermfg=white guibg=#ffd9d9
+"autocmd InsertLeave * match OverLength /\%81v.\+/
+
+" fix slight delay after pressing <Esc>O
+" http://ksjoberg.com/vim-esckeys.html
+set timeout timeoutlen=1000 ttimeoutlen=100
+
+" fix shift-typos :W, :Q
+:command! W w
+:command! Q q
+
+"""
+"   SYSTEM-SPECIFIC
+"""
+
+if has("unix")
+    let $CTAGS = 'ctags'
+
+    if $TERM == "xterm-256color" || $TERM == "screen-256color" || $COLORTERM == "gnome-terminal"
+        set t_Co=256
+   endif
+elseif has("win32")
+    "set guifont=Source\ Sans\ Pro
+    set guifont=Consolas:cEASTEUROPE
+
+    "source $VIMRUNTIME/mswin.vim
+    "behave mswin
+
+    cd d:/projects
+    "cd /media/sf_D/projects
+
+    " cygwin
+    set shell=C:/cygwin/bin/bash
+    set shellcmdflag=--login\ -c
+    set shellxquote=\"
+
+    let $HOME = $USERPROFILE
+    let $VIM = "C:/Program Files (x86)/Vim"
+endif
+
+"""
+"   USEFUL AUTOCMDS
+"""
+
+" EOL whitespaces highlighting
+highlight default link EndOfLineSpace ErrorMsg
+match EndOfLineSpace / \+$/
+autocmd InsertEnter * hi link EndOfLineSpace Normal
+autocmd InsertLeave * hi link EndOfLineSpace ErrorMsg
+
+"""
+"   CODE COMPLETION
+"""
+
+set ofu=syntaxcomplete#Complete
+
+function! CleverTab()
+    if pumvisible()
+        return "\<C-N>"
+    endif
+    if strpart( getline('.'), 0, col('.')-1 ) =~ '^\s*$'
+        return "\<Tab>"
+    elseif exists('&omnifunc') && &omnifunc != ''
+        return "\<C-X>\<C-O>"
+    else
+        return "\<C-N>"
+    endif
+endfunction
+
+inoremap <Tab> <C-R>=CleverTab()<CR>
+
+" Typos
+cabbr Lwq wq
+cabbr qw wq
+cabbr swq wq
+cabbr Wq wq
+
