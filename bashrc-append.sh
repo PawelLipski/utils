4200bc87d7a77bcfa44ee26346eb7fda4ea9b1f2
Pawel Lipski <pawel.p.lipski@gmail.com>
2017-05-23 14:27:38 +0200

    Fix PS1

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -212,7 +212,7 @@ set_up_prompt() {
     local user_and_host="\[\033[01;32m\]\u@\h"
     local cur_location="\[\033[01;34m\]\w"
     local git_branch_color="\[\033[31m\]"
-    local git_branch=`@`
+    local git_branch=`current-branch`
     local prompt_tail="\[\033[35m\]$"
     local last_color="\[\033[00m\]"
     export PS1="$user_and_host $cur_location ${git_branch_color}${git_branch}${prompt_tail}${last_color} "
