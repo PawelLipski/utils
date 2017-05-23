5a7ba285c727328eb54afdfb5d04310416a1c390
Pawel Lipski <pawel.p.lipski@gmail.com>
2017-05-23 14:25:44 +0200

    Set up prompt

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -208,6 +208,16 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 export CDPATH='.:~'
 
+set_up_prompt() {
+    local user_and_host="\[\033[01;32m\]\u@\h"
+    local cur_location="\[\033[01;34m\]\w"
+    local git_branch_color="\[\033[31m\]"
+    local git_branch=`@`
+    local prompt_tail="\[\033[35m\]$"
+    local last_color="\[\033[00m\]"
+    export PS1="$user_and_host $cur_location ${git_branch_color}${git_branch}${prompt_tail}${last_color} "
+}
+set_up_prompt
 
 # sbt opts and aliases
 
