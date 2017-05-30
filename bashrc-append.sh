7b3c6f6b42786fb6606a9192a44b5bdb23ba3c13
Pawel Lipski <pawel.p.lipski@gmail.com>
2017-05-30 10:41:59 +0100

    Fix grbio

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -110,7 +110,7 @@ ginit() {
 }
 
 grbio() {
-	target_base_branch=`git branch | grep "  $1"`
+	target_base_branch=${1-@{-1\}}
 	latest_excluded_commit=${2-@~}
 	git rebase -i --onto $target_base_branch $latest_excluded_commit $(current-branch)
 }
