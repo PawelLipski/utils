00868904e6ef3aaf2a0f8a0ad246ee4f7ab1f97a
Pawel Lipski <pawel.p.lipski@gmail.com>
2018-01-02 10:40:21 +0100

    Fix prompt

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -241,7 +241,7 @@ set_up_prompt() {
     local user_and_host="\[\033[01;32m\]\u@\h"
     local cur_location="\[\033[01;34m\]\w"
     local git_branch_color="\[\033[31m\]"
-    local git_branch='`git status 2>/dev/null >/dev/null && current-branch`'
+    local git_branch='`git status 2>/dev/null >/dev/null && git @`'
     local prompt_tail="\[\033[35m\]$"
     local last_color="\[\033[00m\]"
     export PS1="$user_and_host ${cur_location} ${git_branch_color}${git_branch}${prompt_tail}${last_color} "
--- reload.sh
+++ reload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 command cp bashrc-append.sh ~/.bashrc-append.sh
+command cp git-stack ~/bin/git-stack
 . ~/.bashrc-append.sh
 command cp gitconfig ~/.gitconfig
 command cp vimrc ~/.vimrc
