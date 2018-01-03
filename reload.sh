678b407c6f5a8a7da684a899f9a9cbeac32e1c00
Pawel Lipski <pawel.p.lipski@gmail.com>
2018-01-03 11:03:39 +0100

    Add git stems

--- reload.sh
+++ reload.sh
@@ -2,6 +2,7 @@
 
 command cp bashrc-append.sh ~/.bashrc-append.sh
 command cp git-stack ~/bin/git-stack
+command cp git-stems ~/bin/git-stems
 . ~/.bashrc-append.sh
 command cp gitconfig ~/.gitconfig
 command cp vimrc ~/.vimrc
