5717c6772dc18834cf1ce8b4f06ac5c38b334838
Pawel Lipski <pawel.p.lipski@gmail.com>
2018-01-03 18:54:22 +0100

    Ladder part 1

--- reload.sh
+++ reload.sh
@@ -2,7 +2,7 @@
 
 command cp bashrc-append.sh ~/.bashrc-append.sh
 command cp git-stack ~/bin/git-stack
-command cp git-stems ~/bin/git-stems
+command cp git-ladder ~/bin/git-ladder
 . ~/.bashrc-append.sh
 command cp gitconfig ~/.gitconfig
 command cp vimrc ~/.vimrc
