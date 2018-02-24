a21b44a39382b73cfc538294021d2383e4a37fb4
Pawel Lipski <pawel.p.lipski@gmail.com>
2018-02-24 14:39:56 +0100

    Split into other repos

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -143,10 +143,14 @@ ginit() {
 	git commit -m 'Initial commit'
 }
 
+function g@ () {
+	git symbolic-ref --short HEAD
+}
+
 grbo() {
 	target_base_branch=${1}
 	latest_excluded_commit=${2}
-	git rebase -i --onto $target_base_branch $latest_excluded_commit `git @`
+	git rebase -i --onto $target_base_branch $latest_excluded_commit `g@`
 }
 
 blamestat() {
@@ -238,7 +242,7 @@ set_up_prompt() {
     local user_and_host="\[\033[01;32m\]\u@\h"
     local cur_location="\[\033[01;34m\]\w"
     local git_branch_color="\[\033[31m\]"
-    local git_branch='`git status 2>/dev/null >/dev/null && git @`'
+    local git_branch='`git status 2>/dev/null >/dev/null && g@`'
     local prompt_tail="\[\033[35m\]$"
     local last_color="\[\033[00m\]"
     export PS1="$user_and_host ${cur_location} ${git_branch_color}${git_branch}${prompt_tail}${last_color} "
--- reload.sh
+++ reload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 command cp bashrc-append.sh ~/.bashrc-append.sh
-command cp git-machete ~/bin/
 . ~/.bashrc-append.sh
 command cp gitconfig ~/.gitconfig
 command cp vimrc ~/.vimrc
