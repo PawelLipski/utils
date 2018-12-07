b11f717fba6619892cf9ffedd78f1340c7eef53a
Pawel Lipski <pawel.p.lipski@gmail.com>
2018-12-07 19:49:37 +0100

    Update @s with submodule

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -90,7 +90,7 @@ alias @e='git machete edit'
 alias @gd='git machete go down'
 alias @gu='git machete go up'
 alias @r='git machete reapply'
-alias @s='git machete status'
+alias @s='git machete status; git submodule foreach "git machete status"'
 alias @sl='git machete status -l'
 alias @sq="GIT_SEQUENCE_EDITOR='sed -i \"1s/^pick/reword/;2,\\\$s/^pick/fixup/\"' git machete reapply" # squash
 alias @t='GIT_SEQUENCE_EDITOR=: git machete traverse' # traverse without interactive editor
