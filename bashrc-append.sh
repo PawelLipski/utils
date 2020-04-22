17113179ad6914200810973f60955b0da41aa7e0
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-04-22 11:10:29 +0200

    Add hcis

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -131,6 +131,7 @@ alias gsms='git submodule status'
 alias gsmu='git submodule update'
 alias gx='git stash'
 alias gxa='git stash apply'
+alias hcis='hub ci-status'
 
 function @sq {
     GIT_SEQUENCE_EDITOR="sed -i '1s/^pick/reword/;2,\$s/^pick/fixup/'" git machete reapply
