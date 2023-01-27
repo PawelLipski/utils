22940c8e26152c461f7d20753dc5eba39d02de0d
Pawel Lipski <pawel.p.lipski@gmail.com>
2023-01-27 18:10:04 +0100

    CLICOLORS

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -8,8 +8,9 @@ if [ -x /usr/bin/dircolors ]; then
   alias grep='grep --color=auto'
   alias fgrep='fgrep --color=auto'
   alias egrep='egrep --color=auto'
+else
+  export CLICOLOR=YES
 fi
-
 
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
