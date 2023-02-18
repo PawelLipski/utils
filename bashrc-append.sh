5f1a8d132b54fcec1b6bb9cb5dd185c1db85d731
Pawel Lipski <pawel.p.lipski@gmail.com>
2023-02-18 20:42:20 +0100

    Fix git_machete_anno in PS1

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -320,7 +320,7 @@ function get_git_index_char() {
 function set_up_prompt() {
   local time='$(date +%H:%M)'
   local git_branch='$(cb=$(g@); [[ $cb ]] && echo " <git:$cb>")'
-  local git_machete_anno='$(cb=$(g@); [[ $cb ]] && grep -Po "(?<=^${cb} ).*" $([[ -f .git/machete ]] && echo .git/machete || git machete file))'
+  local git_machete_anno='$(cb=$(g@); [[ $cb ]] && grep -Po "(?<=${cb}).*" $([[ -f .git/machete ]] && echo .git/machete || git machete file))'
   local git_index='$(get_git_index)'
   local prompt_tail=" \[\033[0m\033[01;35m\]\\$\[\033[0m\]"
   local kube_status='$(if [[ ${__display_kube_in_ps1-} ]]; then echo " \[\033[0m\033[1m\]$(kubectx -c):$(kubens -c)"; fi)'
