4654df2cf8820e6f8dca230e7b9f47dbc879ae93
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-02-23 21:23:16 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ set -e -u
 u=$HOME/utils
 git clone https://github.com/PawelLipski/utils.git $u
 echo -e "\nsource $u/bashrc-append.sh" >> ~/.bashrc
-ln -s $u/gitconfig ~/.inputrc
+ln -s $u/gitconfig ~/.gitconfig
 ln -s $u/inputrc ~/.inputrc
 ln -s $u/vimrc ~/.vimrc
