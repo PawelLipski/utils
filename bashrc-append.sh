f68dd0f92c8be6fbc2b60eb07cb425eec2ff1490
Pawel Lipski <pawel.p.lipski@gmail.com>
2024-04-15 17:24:30 +0200

    Patch cdiff

--- bashrc-append.sh
+++ bashrc-append.sh
@@ -261,9 +261,7 @@ function .. {
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-function cdiff() {
-  colordiff -u "$@" | less -r
-}
+alias cdiff='colordiff -u'
 
 function check-links() {
   git ls-files \
